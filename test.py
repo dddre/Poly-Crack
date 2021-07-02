8ba0ae128f90517aa57aad28cdc53409c8e8a739
HSEmathprog <dddre@yandex.ru>
2021-07-02 13:09:07 +0300

    finding irreducible polynomes

--- test.py
+++ test.py
@@ -1,30 +1,33 @@
 import berl
 import random
 
+# p = 3
+# f = berl.Poly([berl.FField(x,p) for x in [1, -1, 0, 1]], p)
+# g = berl.Poly([berl.FField(x,p) for x in [1, -1, 0, 2]], p)
+# h = berl.Poly([berl.FField(x,p) for x in [1, 2]], p)
+# print('f={};  g={}; h={}'.format(f,g,h))
+# ff = f*g*g*h*h
+# print(ff)
+# print(berl.berlekamp(ff))
+# ff = f*g*g*h*h*h
+# print(ff)
+# print(berl.berlekamp(ff))
+
+# print('fgh', f * g * h)
+
+
+# for test in range(10):
+#     p = 11
+#     coefs = [berl.FField(random.randrange(0, p), p) for i in range(11)]
+#     poly = berl.Poly(coefs, p)
+#     b = berl.berlekamp(poly)
+#     mul = poly / poly
+#     for pol, pw in b:
+#         mul *= pol ** pw
+#     # print(poly, mul)
+#     assert len(poly / mul) == 1
+#     print("TEST {} OK".format(test))
 p = 3
-f = berl.Poly([berl.FField(x,p) for x in [1, -1, 0, 1]], p)
-g = berl.Poly([berl.FField(x,p) for x in [1, -1, 0, 2]], p)
-h = berl.Poly([berl.FField(x,p) for x in [1, 2]], p)
-print('f={};  g={}; h={}'.format(f,g,h))
-ff = f*g*g*h*h
-print(ff)
-print(berl.berlekamp(ff))
-ff = f*g*g*h*h*h
-print(ff)
-print(berl.berlekamp(ff))
-
-print('fgh', f * g * h)
-
-
-for test in range(1000):
-    p = 11
-    coefs = [berl.FField(random.randrange(0, p), p) for i in range(11)]
-    poly = berl.Poly(coefs, p)
-    b = berl.berlekamp(poly)
-    mul = poly / poly
-    for pol, pw in b:
-        mul *= pol ** pw
-    # print(poly, mul)
-    assert len(poly / mul) == 1
-    print("TEST {} OK".format(test))
-    
+f = berl.Poly([berl.FField(x,p) for x in [0]*(p**5)+[1]], p)
+print(f)
+print(berl.berlekamp(f))
